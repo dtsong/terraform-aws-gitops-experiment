8ee3dcd50e7841c3eb07e138ee216772a22d921b
Daniel Song <xdtsong@gmail.com>
2022-02-28 12:05:02 -0800

    Add backend.conf and GitHub actions stages

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,11 +8,7 @@ terraform {
 
   required_version = ">=0.14.9"
 
-  backend "s3" {
-    bucket = var.s3_bucket_name
-    key    = var.s3_bucket_key
-    region = var.region
-  }
+  backend "s3" {}
 }
 
 provider "aws" {
